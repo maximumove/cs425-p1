e15889935ed4bd389cf309fd6abb026b308abeb9
Troy Berhow Jr <troyberhow@u.boisestate.edu>
2026-09-08 17:38:15 +0000

    added usage message

--- src/main.c
+++ src/main.c
@@ -10,12 +10,18 @@
 
 int main(void)
 {
-    char *greeting = get_greeting("World");
-    if (greeting) {
-        printf("%s\n", greeting);
-        free(greeting); // Free the allocated memory for the greeting
-    } else {
-        printf("Failed to create greeting.\n");
-    }
+    //usage message displayed to user
+    char *useMsg = "Usage: myapp -f <from> -t <to> [-s subject] [-b body] [-p port]\n"
+                       "         [-H helo-host] <server>\n"
+                       "\n"
+                       "-f <from>       envelope sender, for example you@example.com\n"
+                       "-t <to>         envelope recipient\n"
+                       "-s <subject>    subject line (default: empty)\n"
+                       "-b <body>       message body (default: read from stdin)\n"
+                       "-p <port>       port or service name (default: 25)\n"
+                       "-H <helo-host>  host name sent with HELO (default: localhost)\n"
+                       "<server>        host name or address of the mail server\n";
+    
+    printf("%s\n", useMsg);
     return 0;
 }
